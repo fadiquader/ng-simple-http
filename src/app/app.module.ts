c73db3410a60a93522cb8e6c0af8ce21a064cff6
fadi quader <fadi.quader@gmail.com>
2018-08-12 10:35:03 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import {HttpClientModule} from '@angular/common/http';
+import { SearchBoxComponent } from './posts/search-box/search-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    SearchBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'API_URL',
+      useValue: 'https://jsonplaceholder.typicode.com'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
